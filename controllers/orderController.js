8211cee43f710c5851cd7f581ff93eec46c764f3
Raja <srajasep27@gmail.com>
2023-10-17 22:24:35 +0530

    view and edit order api's developed, partially finished aggregate operation for invoice generation

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -5,7 +5,6 @@ const AppError = require('./../utils/appError');
 
 exports.createOrder = catchAsync(async (req, res, next) => {
 	const client = await Client.find({ businessName: req.body.businessName });
-	const idStr = client[0]._id.toString();
 	const order = await Order.create({
 		businessName: req.body.businessName,
 		clientId: client[0]._id.toString(),
@@ -13,6 +12,24 @@ exports.createOrder = catchAsync(async (req, res, next) => {
 		numberOfHeads: req.body.numberOfHeads,
 		orderDate: req.body.orderDate,
 	});
+
+	res.status(200).json({
+		status: 'success',
+		data: {
+			order: order,
+		},
+	});
+});
+
+exports.editOrder = catchAsync(async (req, res, next) => {
+	const order = await Order.findByIdAndUpdate(req.params.id, req.body, {
+		new: true,
+		runValidators: true,
+	});
+
+	if (!order) {
+		return next(new AppError('No order found with that id', 404));
+	}
 
 	res.status(200).json({
 		status: 'success',
@@ -38,19 +55,49 @@ exports.deleteOrder = catchAsync(async (req, res, next) => {
 });
 
 exports.viewOrders = catchAsync(async (req, res, next) => {
-	console.log(req.params);
-	const client = await Client.find({ businessName: req.params.businessName });
-	console.log(client[0]);
 	const orders = await Order.find({ businessName: req.params.businessName });
 
 	if (!orders) {
-		return next(new AppError('No order found with that id', 404));
+		return next(new AppError('No orders found for that client', 404));
 	}
 
 	res.status(200).json({
 		status: 'success',
 		data: {
 			orders: orders,
+		},
+	});
+});
+
+exports.getOrderDetails = catchAsync(async (req, res, next) => {
+	const order = await Order.findById(req.params.id);
+
+	if (!order) {
+		return next(new AppError('No order found with that id', 404));
+	}
+
+	res.status(200).json({
+		status: 'success',
+		data: {
+			order: order,
+		},
+	});
+});
+
+exports.getInvoiceDetails = catchAsync(async (req, res, next) => {
+	const invoice = await Order.aggregate([
+		{
+			$group: {
+				_id: '$foodItem',
+				quantity: { $sum: '$numberOfHeads' },
+			},
+		},
+	]);
+
+	res.status(200).json({
+		status: 'success',
+		data: {
+			invoice: invoice,
 		},
 	});
 });
--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -4,5 +4,7 @@ const router = express.Router();
 
 router.route('/').post(orderController.createOrder);
 router.route('/:businessName').get(orderController.viewOrders);
+router.route('/invoice/:businessName').get(orderController.getInvoiceDetails);
+router.route('/order/:id').get(orderController.getOrderDetails).patch(orderController.editOrder).delete(orderController.deleteOrder);
 
 module.exports = router;
