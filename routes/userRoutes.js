050ec41d650b606e09aee21831ea0772e773390f
Raja <srajasep27@gmail.com>
2024-01-07 12:11:15 +0530

    update user password added

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,7 +8,7 @@ router.post('/login', authController.login);
 router.post('/forgotPassword', authController.forgotPassword);
 router.post('/resetPassword/:token', authController.resetPassword);
 
-router.route('/').get(authController.protect, authController.restrictTo('admin'), userController.getAllUsers);
+router.route('/').get(authController.protect, authController.restrictTo('admin'), userController.getAllUsers).patch(authController.protect, authController.updateUserPassword);
 router.route('/:id').delete(authController.protect, authController.restrictTo('admin'), userController.removeUser);
 
 module.exports = router;
